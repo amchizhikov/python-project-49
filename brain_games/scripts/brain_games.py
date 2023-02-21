3431cc13d5b8f1b9b0eb83fb66a5edfa7ea79cd1
Alexander <chizhik036@gmail.com>
2023-02-22 02:22:49 +0700

    update files

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+import prompt
 def main():
     print("Welcome to the Brain Games!")
 
 
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+
 
 if __name__ == '__main__':
     main()
+    welcome_user()
